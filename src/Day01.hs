c597a33db66d2588a581c836b38b66823c579ccc
Andres Crucetta Nieto <acrucettanieto@gmail.com>
2024-12-01 18:50:12 -0500

    fix: fixed lsp

--- src/Day01.hs
+++ src/Day01.hs
@@ -1,10 +1,8 @@
 module Day01 where
 
 import Paths_aoc (getDataFileName)
-import Data.List.Split (splitOn)
-import Data.List (sort, transpose, nub)
+import Data.List (sort, transpose)
 import Debug.Trace
-import Data.Char
 
 {-
 Steps:
@@ -18,14 +16,14 @@ readInts :: [[String]] -> [[Int]]
 readInts = map (map read)
 
 absDiff :: Num a => a -> a -> a
-absDiff = \x y -> abs (x-y)
+absDiff x y = abs (x-y)
 
 solve1 :: [Char] -> Int
 solve1 input = do
   let groups = transpose $ readInts $ map words (lines input)
   let (g1, g2) = (sort $ head groups, sort $ last groups)
   let totalDistance = sum $ zipWith absDiff g1 g2
-  trace(show totalDistance) (totalDistance)
+  trace (show totalDistance) totalDistance
 
 {-
 Steps:
@@ -39,7 +37,7 @@ Steps:
 -- Count number of times value N occurs in list Xs
 similarityScore :: Int -> [Int] -> Int
 similarityScore num xs = trace (show result) result
-  where result = num * (length $ filter (==num) xs)
+  where result = num * length (filter (==num) xs)
 
 solve2 :: [Char] -> Int
 solve2 input = do
@@ -47,7 +45,7 @@ solve2 input = do
   let (g1, g2) = (head groups, last groups)
   let similarCounts = map (\x -> similarityScore x g2) g1
   let similarityTotal = sum similarCounts
-  trace(show similarityTotal) (similarityTotal)
+  trace (show similarityTotal) similarityTotal
 
 day01 :: IO ()
 day01 = do
