a792dd773c4fa7e4a8e2dd8c600b359f4ebfd9a4
Andres Crucetta Nieto <acrucettanieto@gmail.com>
2024-12-06 11:51:45 -0600

    refactor(Day06): Improve grid traversal and visited tracking logic
    - Simplified traverseGrid function with clearer parameter handling
    - Added nextPos calculation as a separate where clause
    - Modified solve1 to use nub for unique visited locations
    - Cleaned up import statements

--- src/Day06.hs
+++ src/Day06.hs
@@ -1,11 +1,12 @@
 module Day06 where
 
-import Paths_aoc (getDataFileName)
+import Data.Bifunctor
 import Data.List.Split.Internals (build)
+import Data.Maybe
 import Debug.Trace (trace)
 import Lib
-import Data.Maybe
-import Data.Bifunctor
+import Paths_aoc (getDataFileName)
+import Data.List
 
 {-
 Part 1
@@ -30,27 +31,28 @@ Approach:
       - If it's a . keep the same direction
 -}
 
-
 -- Function to rotate the direction 90 degrees to the right
 rotateRight :: (Int, Int) -> (Int, Int)
 rotateRight (dx, dy) = (dy, -dx)
 
 -- Traverse the grid in a given direction
 traverseGrid :: (Int, Int) -> (Int, Int) -> Grid Char -> [(Int, Int)] -> [(Int, Int)]
-traverseGrid (x, y) (dx, dy) grid visited =
-  case getAt grid (x+dx, y+dy)  of
-    Just '#' -> trace("# -> Current coords:" ++ show (x,y)) traverseGrid (bimap (+x) (+y) (rotateRight(dx,dy))) (rotateRight (dx, dy)) grid visited
-    Just '.' -> trace(". -> Current coords:" ++ show (x,y)) traverseGrid (x+dx, y+dy) (dx, dy) grid ((x, y) : visited)
-    Just '^' -> trace("^ -> Current coords:" ++ show (x,y)) traverseGrid (x+dx, y+dy) (dx, dy) grid ((x, y) : visited) 
-    Nothing  -> visited
+traverseGrid pos@(x, y) dir@(dx, dy) grid visited =
+  case getAt grid nextPos of
+    Just '#' -> trace ("# -> Current coords:" ++ show pos) traverseGrid pos (rotateRight dir) grid (pos : visited)
+    Just '.' -> trace (". -> Current coords:" ++ show pos) traverseGrid nextPos dir grid (pos : visited)
+    Just '^' -> trace ("^ -> Current coords:" ++ show pos) traverseGrid nextPos dir grid (pos : visited)
+    Nothing -> visited
     _ -> error "Unknown character"
+  where
+    nextPos = (x + dx, y + dy)
 
 solve1 :: [String] -> Int
 solve1 input =
   let grid = buildGrid input
-      startCoords = fromMaybe (-1,-1) $ findCoords '^' grid 
-      visited = traverseGrid startCoords (-1,0) grid []
-  in trace(show startCoords ++ show visited) length visited
+      startCoords = fromMaybe (-1, -1) $ findCoords '^' grid
+      visited = traverseGrid startCoords (-1, 0) grid []
+   in trace (show startCoords ++ show visited) (length $ nub visited) + 1
 
 solve2 :: String -> Int
 solve2 input = do
@@ -60,4 +62,5 @@ day06 :: IO ()
 day06 = do
   input <- getDataFileName "day06-input.txt" >>= readFile
   print $ solve1 (lines input)
-  -- print $ solve2 input
+
+-- print $ solve2 input
