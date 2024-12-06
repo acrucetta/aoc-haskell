ac06020d26428780599e27ce778d71af20b9b86a
Andres Crucetta Nieto <acrucettanieto@gmail.com>
2024-12-05 22:32:16 -0600

    refactor(Day05): Improve update validation and solve logic with graph-based approach
    - Renamed `validateUpdate` to `isUpdateValid`
    - Modified `getMiddlePages` to simplify logic
    - Added new `validateUpdate` function using topological sorting
    - Updated `solve2` to use new validation approach
    - Removed unused imports and commented-out code

--- src/Day05.hs
+++ src/Day05.hs
@@ -4,31 +4,10 @@ module Day05 where
 
 import Data.IntSet (IntSet, fromList, isSubsetOf)
 import Data.List.Split (splitOn)
-import Data.Map (valid)
 import Debug.Trace (trace)
 import Paths_aoc (getDataFileName)
 import Lib (strToIntList)
-import GHC.Conc (par)
-
-{-
-Steps:
-- Split the input into the map and the rows of input
-- Make a tuple set of the pairs
-- For each cell in the rows of input, check that all
-other cells after it come after it
-e.g.. 75 -> 75|47, 75|61, 75|53, and 75|29
-
-- Now we will iterate over each cell in the input
-e.g., 75, we will get the next pages, and we will
-check if the "rest" list is contained in the nextPages
-  if not, we return False, else, we keep iterating
-- If we're at the last item, we return True as well
-
-TIP: If we use sets we can just determine if the rest of pages are a subset of
-  the next pages in the rulebook, if so, we're good
-
-- We build an array of Bools and return those values
--}
+import Data.Graph (buildG, topSort)
 
 toTuple :: [String] -> (Int, Int)
 toTuple [a, b] = (read a :: Int, read b :: Int)
@@ -51,36 +30,62 @@ validatePage currUpdate restUpdates rules =
    in remainingPages `isSubsetOf` validPages
 
 -- Check if based on a list of pages, the update is valid
-validateUpdate :: [Int] -> [(Int, Int)] -> Bool
-validateUpdate [] _ = True
-validateUpdate [x] _ = True
-validateUpdate (x : xs) rules = validatePage x xs rules && validateUpdate xs rules
+isUpdateValid :: [Int] -> [(Int, Int)] -> Bool
+isUpdateValid [] _ = True
+isUpdateValid [x] _ = True
+isUpdateValid (x : xs) rules = validatePage x xs rules && isUpdateValid xs rules
 
 getMiddle:: [Int] -> Int
 getMiddle lst = lst !! (length lst `div` 2)
 
-getMiddlePages :: [(Bool,[Int])] -> [Int] -> [Int]
+getMiddlePages :: [[Int]] -> [Int] -> [Int]
 getMiddlePages [] acc = acc
-getMiddlePages [x] acc = if fst x then acc ++ [getMiddle (snd x)] else acc
-getMiddlePages (x:xs) acc = if fst x then getMiddlePages xs (acc ++ [getMiddle (snd x)]) else getMiddlePages xs acc
+getMiddlePages [x] acc = acc ++ [getMiddle x]
+getMiddlePages (x:xs) acc = getMiddlePages xs (acc ++ [getMiddle x])
 
 solve1 :: [Char] -> Int
 solve1 input =
   let parts = splitOn "\n\n" input
       (rules, updates) = (buildRuleset $ words $ head parts, words $ last parts)
       parsedUpdates = map (strToIntList . splitOn ",") updates
-      validUpdates = map (\update -> validateUpdate update rules) parsedUpdates
-      updatesWithFlag = zip validUpdates parsedUpdates
-      middlePages = getMiddlePages updatesWithFlag []
+      flagList = map (\update -> isUpdateValid update rules) parsedUpdates
+      updatesWithFlag = zip flagList parsedUpdates
+      validUpdates = map snd $ filter fst updatesWithFlag
+      middlePages = getMiddlePages validUpdates []
    in trace (show middlePages) sum middlePages
+
+
+validateUpdate :: [(Int, Int)] -> [Int] -> [Int]
+validateUpdate rules update =
+  let 
+      filteredRules = filter (\(c1, c2) -> c1 `elem` update && c2 `elem` update) rules
+      -- Create edges for the graph from filtered rules
+      edges = [(c1, c2) | (c1, c2) <- filteredRules]
+      -- Find min and max nodes to define graph bounds
+      nodes = concatMap (\(a, b) -> [a, b]) filteredRules
+      minNode = minimum nodes
+      maxNode = maximum nodes
+      graph = buildG (minNode, maxNode) edges
+      sortedUpdate = topSort graph
+      -- Filter sorted nodes to only include those from the original update
+      -- (since graph might include extra nodes from the rules)
+      finalUpdate = filter (`elem` update) sortedUpdate
+   in finalUpdate
 
 solve2 :: [Char] -> Int
 solve2 input = do
-  23
+  let parts = splitOn "\n\n" input
+      (rules, updates) = (buildRuleset $ words $ head parts, words $ last parts)
+      parsedUpdates = map (strToIntList . splitOn ",") updates
+      flagList = map (`isUpdateValid` rules) parsedUpdates
+      updatesWithFlag = zip flagList parsedUpdates
+      invalidUpdates = map snd $ filter (not . fst) updatesWithFlag
+      validatedUpdates = map (validateUpdate rules) invalidUpdates
+      validatedMiddlePages = getMiddlePages validatedUpdates []
+   in sum validatedMiddlePages
 
 day05 :: IO ()
 day05 = do
   input <- getDataFileName "day05-input.txt" >>= readFile
-  print $ solve1 input
-
--- print $ solve2 input
+  -- print $ solve1 input
+  print $ solve2 input
