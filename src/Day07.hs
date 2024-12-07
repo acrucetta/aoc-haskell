27cb4c397ccdc173f1eb0bd490866c655cfeb0c5
Andres Crucetta Nieto <acrucettanieto@gmail.com>
2024-12-07 10:13:10 -0600

    feat(day07): implement solution for day 7 puzzle with number combination validation
    Adds logic to validate number combinations using different operations, including a new concatenation operation. Modifies solve1 and solve2 to find valid target numbers and calculates their sum.

--- src/Day07.hs
+++ src/Day07.hs
@@ -2,38 +2,54 @@
 
 module Day07 where
 
+import qualified Control.Monad
 import Data.List.Split (splitOn)
 import Debug.Trace (trace)
 import Lib (toIntList)
 import Paths_aoc (getDataFileName)
+import Prelude hiding ((||))
+import Data.Char (intToDigit)
 
-{-
-Steps:
+type Op = Int -> Int -> Int
 
-Goal: Figure out if with add and multiply the operators will meet our target number
-We want to classify as True the equations that meet the result and add them.
+partOneOps :: [(String, Op)]
+partOneOps = [("+", (+)), ("*", (*))]
 
-Steps:
-- Parse the input into targer (int) and numbers [Int] -> (Int,[Int])
-- Figure out how many operations we could use (n-1) where n = length of numbers
-- Do some form of fold left with each operator in order,
-figure out how to do this
-- For each operator result, check how much we got and if it matches the target
-  return True
--}
+(||) :: Int -> Int -> Int
+(||) a b = read (show a ++ show b) :: Int
+
+partTwoOps :: [(String, Op)]
+partTwoOps = [("+", (+)), ("*", (*)),("||",(||))]
+
+applySeq :: [(String, Op)] -> [Int] -> Int
+applySeq [] (x : _) = x
+applySeq ((_, op) : ops) (x : y : rest) = applySeq ops (op x y : rest)
+applySeq _ _ = error "No more numbers"
+
+isValid :: (Int, [Int]) -> Bool
+isValid (target, nums) =
+  -- We will have N-1 possible operations given N is the count of numbers
+  let countOps = length nums - 1
+      combinations = Control.Monad.replicateM countOps partTwoOps
+      validCombinations = [applySeq combo nums == target | combo <- combinations]
+   in any id validCombinations
 
 solve1 :: [String] -> Int
 solve1 input =
   let rows = map (splitOn ":") input
       equations = [(read $ head row :: Int, (toIntList . words) $ last row) | row <- rows]
-   in trace (show equations) 23
+      validTargets = map snd $ [(isValid eqn, fst eqn) | eqn <- equations, isValid eqn]
+   in trace (show validTargets) sum validTargets 
 
 solve2 :: [String] -> Int
 solve2 input = do
-  23
+  let rows = map (splitOn ":") input
+      equations = [(read $ head row :: Int, (toIntList . words) $ last row) | row <- rows]
+      validTargets = map snd $ [(isValid eqn, fst eqn) | eqn <- equations, isValid eqn]
+   in trace (show validTargets) sum validTargets 
 
 day07 :: IO ()
 day07 = do
   input <- getDataFileName "day07-input.txt" >>= readFile
-  print $ solve1 $ lines input
+  -- print $ solve1 $ lines input
   print $ solve2 $ lines input
